daa199bd50028fdd98ce2881ed44cc05c0e4f8df
Willian-Cueva <enriquewillian2@gmail.com>
2022-04-19 12:47:02 -0500

    proyecto terminado

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Banco Electronico',
       theme: ThemeData(
         primarySwatch: Colors.blue,
